db79d57d420a45691ed8f74b16864a475628899e
Ashish Baraskar <aabaraskar@asu.edu>
2021-04-17 16:55:17 -0500

    Code changes for adding new Product

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.PositiveIntegerField()
+    product_code = models.CharField(max_length=100, default='SOME STRING')
+    product_name = models.CharField(max_length=50, default='SOME STRING')
+    product_desc = models.CharField(max_length=100, default='SOME STRING')
+    specifications = models.CharField(max_length=100, default='SOME STRING')
+    manufacturer_id = models.PositiveIntegerField()
+    substrate_manufacturer = models.CharField(max_length=50, default='SOME STRING')
+    substrate_type = models.CharField(max_length=50, default='SOME STRING')
+    superstate_manufacturer = models.CharField(max_length=50, default='SOME STRING')
+    superstate_type = models.CharField(max_length=50, default='SOME STRING')
+    interconnect_supplier = models.CharField(max_length=50, default='SOME STRING')
+    interconnect_material = models.CharField(max_length=50, default='SOME STRING')
+    fuse_rating = models.PositiveIntegerField()
+    bypass_diodes = models.PositiveIntegerField()
+    series_strings = models.PositiveIntegerField()
+    cells_series = models.PositiveIntegerField()
+    total_cells = models.PositiveIntegerField()
+    cell_technology = models.CharField(max_length=50, default='SOME STRING')
+    cell_area = models.PositiveIntegerField()
+    weight = models.PositiveIntegerField()
+    width = models.PositiveIntegerField()
+    rated_voc = models.CharField(max_length=100, default='SOME STRING')
+    rated_isc = models.CharField(max_length=100, default='SOME STRING')
+    rated_vmp = models.CharField(max_length=100, default='SOME STRING')
+    rated_imp = models.CharField(max_length=100, default='SOME STRING')
+    rated_pmp = models.CharField(max_length=100, default='SOME STRING')
+    rated_ff = models.CharField(max_length=100, default='SOME STRING')
+    length_str = models.CharField(max_length=30, default='SOME STRING')
+
+    def __str__(self):
+        return self.product_name
+
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100, default='SOME STRING')
@@ -10,3 +43,5 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import ProductSerializer
+from .models import Product
+
 # Create your views here.
+@api_view(['GET'])
+def productOverview(request):
+    product_urls = {
+        'List': '/product-list/',
+        'Detail View': '/product-detail/<int:id>',
+        'Create': '/product-create/',
+        'Update': '/product-update/<int:id>',
+        'Delete': '/product-delete/<int:id>'
+    }
+
+    return Response(product_urls)
+
+@api_view(['GET'])
+def showAll(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def viewProduct(request, pk):
+    product = Product.objects.get(product_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createProduct(request):
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def updateProduct(request, pk):
+    product = Product.objects.get(product_id=pk)
+    serializer = ProductSerializer(product, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def deleteProduct(request, pk):
+    product = Product.objects.get(product_id=pk)
+    product.delete()
+        
+    return Response('Items delete complete.')
